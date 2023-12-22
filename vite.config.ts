8d859e94c03c9d493810463c3a1f127e88f08ef9
Mike Odnis <airwolf635@gmail.com>
2023-12-22 18:30:03 -0500

    Workflow to publish package

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "./src/components/index.tsx"),
-      name: "UiLib",
-      fileName: "ui-lib",
+      name: "ui-library",
+      fileName: "ui-library",
     },
     rollupOptions: {
       external: ["react", "react-dom"],
